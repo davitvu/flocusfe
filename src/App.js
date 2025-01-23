0fb290c8f0d3e2ab218c809deeaa7347d157c842
davitvu <thomanhdiroinoi@gmail.com>
2025-01-23 16:36:59 +0700

    add classNames, sass, router-dom@6, customize cra, babel-plugin-module-resolver, axios

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind()
 
 function App() {
   return (
-    <div className="App">
+    <div className={cx('header')}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Test</h1>
       </header>
     </div>
   );
